35ba75c82a1b8224a6f20df979851075b1afc866
DaisukeDaisuke <DaisukeDaisuke@users.noreply.github.com>
2026-06-18 00:34:39 +0900

    Enable zlib savestate loading

--- webassembly/build.sh
+++ webassembly/build.sh
@@ -76,14 +76,14 @@ OBJECTS=()
 compile_cpp() {
   local src="$1"
   local obj="${BUILD_DIR}/$(echo "${src#${ROOT_DIR}/}" | tr '/\\:' '___').o"
-  emcc "${INCLUDES[@]}" -O3 -std=c++17 -include algorithm -include cassert -sUSE_ZLIB=1 -c "${src}" -o "${obj}"
+  emcc "${INCLUDES[@]}" -O3 -std=c++17 -DHAVE_LIBZ -include algorithm -include cassert -sUSE_ZLIB=1 -c "${src}" -o "${obj}"
   OBJECTS+=("${obj}")
 }
 
 compile_c() {
   local src="$1"
   local obj="${BUILD_DIR}/$(echo "${src#${ROOT_DIR}/}" | tr '/\\:' '___').o"
-  emcc "${INCLUDES[@]}" -O3 -sUSE_ZLIB=1 -c "${src}" -o "${obj}"
+  emcc "${INCLUDES[@]}" -O3 -DHAVE_LIBZ -sUSE_ZLIB=1 -c "${src}" -o "${obj}"
   OBJECTS+=("${obj}")
 }
 
